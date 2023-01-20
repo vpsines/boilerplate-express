aca7a77c412d1daeb29a2b548f75fcfadab03f71
vpsines <sasip6028@gmail.com>
2023-01-21 00:26:04 +0530

    style: add static assets

--- myApp.js
+++ myApp.js
@@ -3,6 +3,7 @@ let app = express();
 console.log("Hello World");
 app.get("/",function(req,res){
     let absolutePath = __dirname + '/views/index.html';
+    app.use('/public',express.static(__dirname + '/public'));
     res.sendFile(absolutePath);
 });
 
