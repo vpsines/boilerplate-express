55f726b0822e5b305276a811007bbd4b7087e71a
vpsines <sasip6028@gmail.com>
2023-01-20 14:07:07 +0530

    refactor: replace world with express

--- myApp.js
+++ myApp.js
@@ -2,7 +2,7 @@ let express = require('express');
 let app = express();
 console.log("Hello World");
 app.get("/",function(req,res){
-    res.send("Hello World");
+    res.send("Hello Express");
 });
 
 
