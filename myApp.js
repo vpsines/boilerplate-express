310ca7cd221cd3712dd2a593d3ba8cdbba72605a
vpsines <sasip6028@gmail.com>
2023-01-21 00:16:07 +0530

    feat: add redirection to index.html

--- myApp.js
+++ myApp.js
@@ -2,7 +2,8 @@ let express = require('express');
 let app = express();
 console.log("Hello World");
 app.get("/",function(req,res){
-    res.send("Hello Express");
+    let absolutePath = __dirname + '/views/index.html';
+    res.sendFile(absolutePath);
 });
 
 
