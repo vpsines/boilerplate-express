8a1bd4814c5b672e7a1df08ad5bbbdef51c7e1a9
vpsines <sasip6028@gmail.com>
2023-01-22 20:04:00 +0530

    feat: add route level req logger

--- myApp.js
+++ myApp.js
@@ -1,7 +1,14 @@
 require('dotenv').config()
 let express = require('express');
 let app = express();
-console.log("Hello World");
+
+app.use(function(req,res,next){
+    // log ip
+    let logMessage = req.method +' '+ req.path + ' - '+req.ip;
+    console.log(logMessage);
+    next();
+});
+
 app.get("/",function(req,res){
     let absolutePath = __dirname + '/views/index.html';
     app.use('/public',express.static(__dirname + '/public'));
